327f7b002cb70494522f68234d0224d375f4df62
Helge <ropf@itu.dk>
2019-06-26 10:27:26 +0200

    Pumped up complexity to hopefully change serverity on code inspector

--- check_it4.py
+++ check_it4.py
@@ -4,13 +4,25 @@ def do_it(value):
     val4 = val2 and val3
     val5 = val3 and val4
     val6 = val4 and val5
+    val7 = val5 and val6
+    val8 = val6 and val7
+    val9 = val7 and val8
+    val10 = val8 and val9
+    val11 = val9 and val10
+    val12 = val10 and val11
     if value:
         if val2:
             if val3:
                 if val4:
                     if val5:
                         if val6:
-                            print(value)
+                            if val7:
+                                if val8:
+                                    if val9:
+                                        if val10:
+                                            if val11:
+                                                if val12:
+                                                    print(value)
 
 
 def main():
